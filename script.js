d390b59415314ce10d0e38fc0ef5485ffb2c5f04
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-08-30 09:06:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+// const TodoInput = document.getElementById("TodoInput")
+// const lister = document.getElementById("lister")
+// var Emer = []
+// console.log(TodoInput)
+
+// 
+// function add() {
+// if (TodoInput.value < 4) {
+// alert("Enter Your Task")
+// } else {
+// console.log(TodoInput.value)
+// Emer.push(TodoInput.value)
+// lister.innerHTML += `<li>${TodoInput.value}</li>`
+// TodoInput.value = ""
+// 
+// }
+// }
+
+const arr = ["hussain shamim", "web developer", "vedio Edditor", "Vfx reder"]
+arr.forEach(element => {
+    console.log(element)
+});
+
+let ele = 0;
+async function abc() {
+     let tim =  setInterval((eem) => {
+            console.log("reder",ele++)
+            if(eem === 1){
+                alert("red")
+            }
+        }, 600)
+        
+    }
+// abc()
+
+
+
+
